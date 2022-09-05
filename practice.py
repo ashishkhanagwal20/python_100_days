fdc67d5027baac936f72d5e9dc33f6e909f15105
ashishkhanagwal20 <ashishstudy20@gmail.com>
2022-09-05 21:25:22 +0530

    day7 and day8

--- practice.py
+++ practice.py
@@ -1,10 +1,37 @@
+import math
 
-for number in range(1,101):
-    if number%3 == 0 and number%5 == 0:
-        print("fizzbuzz")
-    elif number%3 == 0:
-        print("fizz")
-    elif number%5==0:
-        print("buzz")
+
+def prime_checker(number):
+    is_prime = True
+    for item in range(2, number):
+        if number % item == 0:
+            is_prime = False
+    if is_prime:
+        print("It's a Prime Number")
     else:
-        print(number)
+        print("It's not a prime number")
+
+
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
+# for number in range(1, 101):
+#     if number % 3 == 0 and number % 5 == 0:
+#         print("fizzbuzz")
+#     elif number % 3 == 0:
+#         print("fizz")
+#     elif number % 5 == 0:
+#         print("buzz")
+#     else:
+#         print(number)
+
+# def paint_calc(height, width, cover):
+#     cans = (height * width) / cover
+#     print(f"{cans} cans are needed")
+#     return True
+#
+#
+# test_h = int(input("Height of wall: "))
+# test_w = int(input("Width of wall: "))
+# coverage = 5
+# paint_calc(height=test_h, width=test_w, cover=coverage)
